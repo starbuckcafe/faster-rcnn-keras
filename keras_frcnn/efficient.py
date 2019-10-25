01161e15878d89382eecbbd8748024d4a85ca3e7
starbuckcafe <f89120@gmail.com>
2019-10-25 15:18:48 +0800

    efficientNet

---
+++ keras_frcnn/efficient.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""VGG16 model for Keras.
+# Reference
+- [Very Deep Convolutional Networks for Large-Scale Image Recognition](https://arxiv.org/abs/1409.1556)
+"""
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import warnings
+from math import ceil
+from keras.models import Model
+from keras.layers import Flatten, Dense, Input, Conv2D, MaxPooling2D, Dropout
+from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, TimeDistributed
+from keras.engine.topology import get_source_inputs
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras import backend as K
+from keras_frcnn.RoiPoolingConv import RoiPoolingConv
+
+def get_weight_path():
+    if K.image_dim_ordering() == 'th':
+        print('pretrained weights not available for efficientNet with theano backend')
+        return
+    else:
+        return
+
+
+def get_img_output_length(width, height):
+    def get_output_length(input_length):
+        return ceil(input_length / 32)
+
+    return get_output_length(width), get_output_length(height)    
+
+
+def classifier_layers(x, input_shape, trainable=False):
+
+    # compile times on theano tend to be very high, so we use smaller ROI pooling regions to workaround
+    # (hence a smaller stride in the region that follows the ROI pool)
+
+    x = TimeDistributed(GlobalAveragePooling2D(name='avg_pool'))(x)
+
+    return x
+
+
+def rpn(base_layers, num_anchors):
+
+    x = Conv2D(512, (3, 3), padding='same', activation='relu', kernel_initializer='normal', name='rpn_conv1')(base_layers)
+
+    x_class = Conv2D(num_anchors, (1, 1), activation='sigmoid', kernel_initializer='uniform', name='rpn_out_class')(x)
+    x_regr = Conv2D(num_anchors * 4, (1, 1), activation='linear', kernel_initializer='zero', name='rpn_out_regress')(x)
+
+    return [x_class, x_regr, base_layers]
+
+
+def classifier(base_layers, input_rois, num_rois, nb_classes=21, trainable=False):
+
+    # compile times on theano tend to be very high, so we use smaller ROI pooling regions to workaround
+
+    if K.backend() == 'tensorflow':
+        pooling_regions = 7
+        input_shape = (num_rois, 7, 7, 1280)
+    elif K.backend() == 'theano':
+        pooling_regions = 7
+        input_shape = (num_rois, 1280, 7, 7)
+
+    out_roi_pool = RoiPoolingConv(pooling_regions, num_rois)([base_layers, input_rois])
+    out = classifier_layers(out_roi_pool, input_shape=input_shape, trainable=True)
+
+    out = TimeDistributed(Flatten())(out)
+
+    out_class = TimeDistributed(Dense(nb_classes, activation='softmax', kernel_initializer='zero'), name='dense_class_{}'.format(nb_classes))(out)
+    # note: no regression target for bg class
+    out_regr = TimeDistributed(Dense(4 * (nb_classes-1), activation='linear', kernel_initializer='zero'), name='dense_regress_{}'.format(nb_classes))(out)
+    return [out_class, out_regr]
